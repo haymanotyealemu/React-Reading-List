42c5ed1b72063f02be06ba16591ee6548a066002
Haymanot W <haymanotalemu21@gmail.com>
2020-10-12 23:23:22 -0400

    deploying

---
+++ client/build/precache-manifest.853e6a519dc00604ba50879cdc760143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eab486674df0d4d30f146519d264215",
+    "url": "/index.html"
+  },
+  {
+    "revision": "149a825336bbd151967a",
+    "url": "/static/css/main.1ef92e19.chunk.css"
+  },
+  {
+    "revision": "a772f5646685c85634d6",
+    "url": "/static/js/2.bdc43e03.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.bdc43e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149a825336bbd151967a",
+    "url": "/static/js/main.b6359a4c.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
